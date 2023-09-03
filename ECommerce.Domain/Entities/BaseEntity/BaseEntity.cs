108b2939650f9f64502e9fde97c76bdb58b702b5
Emre Koc <mrkoc.95@gmail.com>
2023-09-03 14:39:58 +0300

    Added interceptor to SaveAsync method

--- ECommerce.Domain/Entities/BaseEntity/BaseEntity.cs
+++ ECommerce.Domain/Entities/BaseEntity/BaseEntity.cs
@@ -4,4 +4,5 @@ public class BaseEntity<TKey>
 {
     public TKey Id { get; set; }
     public DateTime CreateedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
 }
--- ECommerce.Persistence/Contexts/ECommerceDbContext.cs
+++ ECommerce.Persistence/Contexts/ECommerceDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Entities.BaseEntity;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Persistence.Contexts;
@@ -13,4 +14,20 @@ public class ECommerceDbContext : DbContext
     public DbSet<Product> Products { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<Customer> Customers { get; set; }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var datas = ChangeTracker.Entries<BaseEntity<Guid>>();
+
+        foreach (var data in datas)
+        {
+            _ = data.State switch
+            {
+                EntityState.Added => data.Entity.CreateedDate = DateTime.UtcNow,
+                EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow
+            };
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
 }
